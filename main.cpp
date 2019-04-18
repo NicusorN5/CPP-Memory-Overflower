c58255eabcffdbdcec9883eb2482d14e688f9b6e
Athanatos <3dradspace@gmail.com>
2019-04-18 10:31:02 +0300

    Usage example
    This is an example on how to use my little fun functions xd

---
+++ main.cpp
@@ -0,0 +1,19 @@
+///Example of usage
+
+#include "memory_overflow.h" //Include the very little header that contains our functions.
+int main()
+{
+    /*
+    while(true) //Keeps creating new memory forever (atleast until the program breaks)
+    {
+        FillMemory(); //Call our function.
+    }
+    */
+    /*
+    while(true)
+    {
+        CPUFiller(); //Call the CPUFiller function. Doesn't do really bad things.
+    }
+    */
+    //HardDriveDestroyer("test.txt"); //This one is my favorite :D
+}
